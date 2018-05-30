46a275a7162d1ebb0444e92e64095ef55c2e6f0e
aminur rahman <aminuriub135@gmail.com>
2018-05-30 03:36:55 +0000

    task2c

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 /* Task 1 */
+var heading = document.getElementById("heading");
+console.log(heading.innerText);
 
 
 /* Task 2 using plain JavaScript */
+function addText(){
+    var task2div =document.getElementById("task2a");
+    var paragraph =document.createElement('p');
+    paragraph.innerText="Hello world";
+    task2div.appendChild(paragraph);
+}
 
-
-
+/*Task 2 b
+function changeBGcolor(event){
+    var body = document.getElementsByTagName('body')[0];
+    if(event.taeget.innerText=="Red"){
+        body.style.backgroundColor='red';
+    }
+    if(event.taeget.innerText=="Green"){
+        body.style.backgroundColor='green';
+    }
+}
 /* Task 4 using jQuery */
